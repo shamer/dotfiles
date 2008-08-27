cc8b0d8080c1143b735bf5206af76d5c92a76c81
Stephen Hamer <stephen.hamer@gmail.com>
2008-08-27 00:50:05 -0400

    Added processing of command line arguments to create_symlink script.

--- create_symlink.py
+++ create_symlink.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 # Script downloaded from:
 # http://yiqiang.org/blog/2008/07/04/publishing-your-dotfiles/
-import os
+import os, sys
 home = os.path.abspath(os.environ['HOME'])
 path = os.path.join(home, '.dotfiles') 
 excludes = ['gtk-2.0', 'create_symlink.py']
-for f in os.listdir(path):
+files = None
+if len(sys.argv) > 1:
+    files = sys.argv[1:]
+else:
+    files = os.listdir(path)
+
+for f in files:
     if f.startswith('.'):
         continue
     if f not in excludes:
